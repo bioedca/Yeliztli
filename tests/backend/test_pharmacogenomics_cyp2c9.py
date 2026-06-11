603f0081c29fb77d294ec60f12704ad16161e0b4
bioedca <bioedca@u.northwestern.edu>
2026-06-11 18:11:00 -0500

    style(pgx): format star3 expectations

--- tests/backend/test_pharmacogenomics_cyp2c9.py
+++ tests/backend/test_pharmacogenomics_cyp2c9.py
@@ -92,9 +92,7 @@ _NEW_DIPLOTYPES = {
 
 _STAR3_DIPLOTYPES = {
     **{
-        diplotype: expected
-        for diplotype, expected in _NEW_DIPLOTYPES.items()
-        if "*3" in diplotype
+        diplotype: expected for diplotype, expected in _NEW_DIPLOTYPES.items() if "*3" in diplotype
     },
     "*1/*3": ("Intermediate Metabolizer", 1.0),
     "*2/*3": ("Poor Metabolizer", 0.5),
